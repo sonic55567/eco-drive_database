89112e35e0c9c572ea181c676a2cf4a211dee987
sonic55567 <sonic55567@gmail.com>
2020-05-30 20:12:34 +0800

    change Phase's type from list to integer

--- GetPhase.py
+++ GetPhase.py
@@ -11,19 +11,18 @@ data = open("converted.json","rb").read()
 data_traffic = json.loads(data)
 
 count = 0
-print("Device :")
-for i in data_traffic["Device"]["S059801"].keys() :
-    print(i)
-print("DeciveWeekday :")
-for i in data_traffic["DeviceWeekday"]["S059801"]["1"]["2100"] :
-    print(i)
-print("Traffic :")
-for i in data_traffic["Traffic"]["S059801"].keys() :
-    print(i)
+# change Phase's value from list to integer
+for i in data_index :
+    if "Phase" in data_node[str(i)].keys() :
+        for j in data_node[str(i)]["Phase"].keys() :
+            data_node[str(i)]["Phase"][str(j)] = 1
 
-"""
+
+
+
+
 f = open("node_v9.json","w+",encoding="utf-8")
 f.write(json.dumps(data_node, ensure_ascii=False, indent=1)) 
 f.close()
-"""
+
 
